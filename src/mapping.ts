82b3a1c341830e41faf4b3b5e1a44323571a5ccb
Harshit Singhal <82471455+harry1121-s@users.noreply.github.com>
2022-08-18 19:29:29 +0530

    Quickswap/MMFinance pairs indexed

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,51 +1,174 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { MMFinance, PairCreated } from "../generated/MMFinance/MMFinance"
-import { ExampleEntity } from "../generated/schema"
+import { Quickswap } from "../generated/Quickswap/Quickswap"
+// import { ExampleEntity } from "../generated/schema"
+import { Pair,Token, Reserve } from "../generated/schema"
+import {
+  ERC20
+} from "../generated/MMFinance/ERC20"
+import { log } from '@graphprotocol/graph-ts'
+
+const MM_Finance = "0x7cFB780010e9C861e03bCbC7AC12E013137D47A5";
 
 export function handlePairCreated(event: PairCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let _id = event.params.pair;
+    let _tokenA = event.params.token0;
+    let _tokenB = event.params.token1;
+    let _exchange = "MM Finance";
+    let _router = "0x51aba405de2b25e5506dea32a6697f450ceb1a17";
+    let _pairId = event.params.param3;
+    log.debug("PE - ID: {}\nTokenA: {}\nTokenB: {}\nPairID: {}", [_id.toHexString(),_tokenA.toHexString(),_tokenB.toHexString(),_pairId.toString()]);
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    // Create Token Instance A
+    let _erc20A = ERC20.bind(_tokenA);
+    let _namecallA = _erc20A.try_name();
+    let _symbolcallA = _erc20A.try_symbol();
+    let _decimalcallA = _erc20A.try_decimals();
+    
+    // Create Token A instance
+    let tokenA =  Token.load(_tokenA.toHex());
+    if(tokenA === null) {
+      tokenA = new Token(_tokenA.toHex());
+      if(_namecallA.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenA.name = _namecallA.value;
+      }
+      if(_symbolcallA.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenA.symbol = _symbolcallA.value;
+      }
+      if(_decimalcallA.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenA.decimal = BigInt.fromI64(_decimalcallA.value);
+      }
+      // tokenA.tokenId = _tokenB.toHexString();
+    }
+    tokenA.save();
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+    // Create Token Instance B
+    let _erc20B = ERC20.bind(_tokenB);
+    let _namecallB = _erc20B.try_name();
+    let _symbolcallB = _erc20B.try_symbol();
+    let _decimalcallB = _erc20B.try_decimals();
+    
+    // Create Token A instance
+    let tokenB =  Token.load(_tokenB.toHex());
+    if(tokenB === null) {
+      tokenB = new Token(_tokenB.toHex());
+      if(_namecallB.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenB.name = _namecallB.value;
+      }
+      if(_symbolcallB.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenB.symbol = _symbolcallB.value;
+      }
+      if(_decimalcallB.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenB.decimal = BigInt.fromI64(_decimalcallB.value);
+      }
+      // tokenB.tokenId = tokenA.id;
+    }
+    tokenB.save();
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.token0 = event.params.token0
-  entity.token1 = event.params.token1
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.INIT_CODE_HASH(...)
-  // - contract.allPairs(...)
-  // - contract.allPairsLength(...)
-  // - contract.createPair(...)
-  // - contract.feeTo(...)
-  // - contract.feeToSetter(...)
-  // - contract.getPair(...)
+    // Create Token Pair
+    let pair = Pair.load(_id.toHex());
+    if(pair === null) {
+      pair = new Pair(_id.toHex());
+      pair.tokenA = _tokenA.toHexString();
+      pair.tokenAEntity = tokenA.id;
+      pair.tokenB = _tokenB.toHexString()
+      pair.tokenBEntity = tokenB.id;
+      pair.exchange = _exchange;
+      pair.router = _router;
+    }
+    pair.save();
 }
+
+export function handlePairCreatedQuickswap(event: PairCreated): void {
+  let _id = event.params.pair;
+    let _tokenA = event.params.token0;
+    let _tokenB = event.params.token1;
+    let _exchange = "Quickswap";
+    let _router = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff";
+    let _pairId = event.params.param3;
+    log.debug("PE - ID: {}\nTokenA: {}\nTokenB: {}\nPairID: {}", [_id.toHexString(),_tokenA.toHexString(),_tokenB.toHexString(),_pairId.toString()]);
+
+    // Create Token Instance A
+    let _erc20A = ERC20.bind(_tokenA);
+    let _namecallA = _erc20A.try_name();
+    let _symbolcallA = _erc20A.try_symbol();
+    let _decimalcallA = _erc20A.try_decimals();
+    
+    // Create Token A instance
+    let tokenA =  Token.load(_tokenA.toHex());
+    if(tokenA === null) {
+      tokenA = new Token(_tokenA.toHex());
+      if(_namecallA.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenA.name = _namecallA.value;
+      }
+      if(_symbolcallA.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenA.symbol = _symbolcallA.value;
+      }
+      if(_decimalcallA.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenA.decimal = BigInt.fromI64(_decimalcallA.value);
+      }
+      // tokenA.tokenId = _tokenB.toHexString();
+    }
+    tokenA.save();
+
+    // Create Token Instance B
+    let _erc20B = ERC20.bind(_tokenB);
+    let _namecallB = _erc20B.try_name();
+    let _symbolcallB = _erc20B.try_symbol();
+    let _decimalcallB = _erc20B.try_decimals();
+    
+    // Create Token A instance
+    let tokenB =  Token.load(_tokenB.toHex());
+    if(tokenB === null) {
+      tokenB = new Token(_tokenB.toHex());
+      if(_namecallB.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenB.name = _namecallB.value;
+      }
+      if(_symbolcallB.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenB.symbol = _symbolcallB.value;
+      }
+      if(_decimalcallB.reverted){
+        log.error("PE - Bind Call Error: Function Name for Token: {}",[_tokenA.toHexString()]);
+      } else {
+        tokenB.decimal = BigInt.fromI64(_decimalcallB.value);
+      }
+      // tokenB.tokenId = tokenA.id;
+    }
+    tokenB.save();
+
+    // Create Token Pair
+    let pair = Pair.load(_id.toHex());
+    if(pair === null) {
+      pair = new Pair(_id.toHex());
+      pair.tokenA = _tokenA.toHexString();
+      pair.tokenAEntity = tokenA.id;
+      pair.tokenB = _tokenB.toHexString()
+      pair.tokenBEntity = tokenB.id;
+      pair.exchange = _exchange;
+      pair.router = _router;
+    }
+    pair.save();
+}
+
